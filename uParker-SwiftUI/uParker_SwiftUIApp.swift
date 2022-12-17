62efd350bff6c2d50656708105da717897ea5b16
Ryan Smetana <ryan.smetana@xavware.com>
2022-12-16 21:44:35 -0500

    Initial Commit

---
+++ uParker-SwiftUI/uParker_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  uParker_SwiftUIApp.swift
+//  uParker-SwiftUI
+//
+//  Created by Smetana, Ryan on 12/16/22.
+//
+
+import SwiftUI
+
+@main
+struct uParker_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
