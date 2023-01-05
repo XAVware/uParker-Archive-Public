287257ddb4d6935d28151a17fc9c065af7549223
Ryan Smetana <ryan.smetana@xavware.com>
2023-01-05 15:00:49 -0500

    Map View changes

--- uParker-SwiftUI/Views/ParkerView.swift
+++ uParker-SwiftUI/Views/ParkerView.swift
@@ -13,70 +13,87 @@ struct ParkerView: View {
     
     @State var isShowingLoginModal: Bool = true
     
+    let tabBarHeight: CGFloat = 60
+    
     // MARK: - BODY
     var body: some View {
-        TabView {
-            ParkingMapViewWrapper()
-                .tabItem {
-                    Text("Park")
+        GeometryReader { geometry in
+            TabView {
+                ZStack {
+                    ParkingMapViewWrapper()
+                    Rectangle()
+                        .frame(width: geometry.size.width, height: tabBarHeight)
+                        .foregroundColor(.white)
+                }
+                    .edgesIgnoringSafeArea(.all)
+                    .tabItem {
+                        
+                        Text("Park")
+                        
+                        Image(systemName: "car.fill")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 30)
+                            .padding(.top)
+                            
+                    }
+                
+                Text("Reservations")
+                    .tabItem {
+                        Text("Reservations")
+                        
+                        Image(systemName: "calendar")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 30)
+                            .padding(.top)
+                    }
+                
+                Text("List View")
+                    .tabItem {
+                        Text("List View")
+                        
+                        Image(systemName: "list.bullet")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 30)
+                            .padding(.top)
+                    }
+                
+                Text("Chat")
+                    .tabItem {
+                        Text("Chat")
+                        
+                        Image(systemName: "bubble.left.fill")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 30)
+                            .padding(.top)
+                    }
+                
+                
+                VStack {
+                    Text("Login/Profile/Settings View")
+                    Button {
+                        self.sessionManager.userType = .host
+                    } label: {
+                        Text("Change to host")
+                    }
                     
-                    Image(systemName: "car.fill")
+                } //: VStack
+                .tabItem {
+                    Text("Login")
+                    
+                    Image(systemName: "person.circle")
                         .resizable()
                         .scaledToFit()
                         .frame(width: 30)
-                }
-            
-            Text("Events")
-                .tabItem {
-                    Text("Events")
-                    
-                    Image(systemName: "flag.2.crossed")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 30)
-                }
-            
-            Text("List View")
-                .tabItem {
-                    Text("List View")
-                    
-                    Image(systemName: "list.bullet")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 30)
-                }
-            
-            Text("Chat")
-                .tabItem {
-                    Text("Chat")
-                    
-                    Image(systemName: "bubble.left.fill")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 30)
-                }
-            
-            
-            VStack {
-                Text("Login/Profile/Settings View")
-                Button {
-                    self.sessionManager.userType = .host
-                } label: {
-                    Text("Change to host")
+                        .padding(.top)
                 }
                 
-            } //: VStack
-            .tabItem {
-                Text("Login")
-                
-                Image(systemName: "person.circle")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 30)
             }
-            
-            
-        }
+        } //: TabView
+        
     }
 }
 
