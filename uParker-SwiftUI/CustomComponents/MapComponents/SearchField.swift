3b9cf500d912e0dc58cf9d973ab16a6b7229ada6
Ryan Smetana <ryan.smetana@xavware.com>
2023-01-09 10:41:12 -0500

    Refactor search box

---
+++ uParker-SwiftUI/CustomComponents/MapComponents/SearchField.swift
@@ -0,0 +1,68 @@
+//
+//  SearchField.swift
+//  uParker-SwiftUI
+//
+//  Created by Smetana, Ryan on 1/9/23.
+//
+
+import SwiftUI
+
+struct SearchField: View {
+    let iconSize: CGFloat = 15
+    let searchBarHeight: CGFloat = 50
+    
+    var body: some View {
+        HStack {
+            Image(systemName: "magnifyingglass")
+                .resizable()
+                .scaledToFit()
+                .frame(width: iconSize)
+            
+            VStack(alignment: .leading) {
+                Text("Where to?")
+                    .font(.title3)
+                    .fontWeight(.semibold)
+                
+                Text("Beaver Stadium - Today")
+                    .font(.caption)
+            } //: VStack
+            .fontDesign(.rounded)
+            .padding(.horizontal)
+            
+            Spacer()
+            
+            Button {
+                //
+            } label: {
+                Image(systemName: "slider.horizontal.3")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: iconSize)
+            }
+            .frame(width: 35, height: 35)
+            .overlay(Circle().stroke(.gray))
+        } //: HStack
+        .padding(.horizontal)
+        .frame(height: searchBarHeight)
+        .background(Color.white)
+        .clipShape(Capsule())
+        .shadow(radius: 4)
+        .overlay(
+            RoundedRectangle(cornerRadius: searchBarHeight)
+                .stroke(.gray, lineWidth: 0.5)
+        )
+        .onTapGesture {
+            print("Search Field Clicked")
+        }
+        
+    }
+}
+
+struct SearchField_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchField()
+            .previewLayout(.sizeThatFits)
+//            .background(.blue)
+            .padding()
+    }
+}
--- uParker-SwiftUI/Views/ParkerView.swift
+++ uParker-SwiftUI/Views/ParkerView.swift
@@ -13,12 +13,15 @@ struct ParkerView: View {
     
     @State var isShowingLoginModal: Bool = true
     
+    let tabBarHeight: CGFloat = 49
     let tabViewDividerPadding: CGFloat = 10
     let searchBarHeight: CGFloat = 50
     
     
+    
     // MARK: - BODY
     var body: some View {
+
         GeometryReader { geo in
             TabView {
                 ZStack {
@@ -26,62 +29,18 @@ struct ParkerView: View {
                         .edgesIgnoringSafeArea(.all)
                     
                     VStack(spacing: 0) {
-                        Rectangle()
+                        Color.white
                             .frame(height: geo.safeAreaInsets.top)
-                            .foregroundColor(.white)
-                        
-                        Rectangle()
-                            .frame(height: searchBarHeight)
-                            .foregroundColor(.white)
-                            .padding(.top, 30)
-                            .overlay(
-                                HStack() {
-                                    Image(systemName: "magnifyingglass")
-                                        .resizable()
-                                        .scaledToFit()
-                                        .frame(width: 15)
-                                        .padding(.horizontal)
-                                    
-                                    VStack(alignment: .leading) {
-                                        Text("Where to?")
-                                            .font(.title3)
-                                            .fontWeight(.semibold)
-                                            .fontDesign(.rounded)
-                                        
-                                        Text("Beaver Stadium - Today")
-                                            .font(.caption)
-                                    } //: VStack
-                                    
-                                    Spacer()
-                                    
-                                    Button {
-                                        //
-                                    } label: {
-                                        Image(systemName: "slider.horizontal.3")
-                                            .resizable()
-                                            .scaledToFit()
-                                            .frame(width: 15)
-                                            .padding(.horizontal)
-                                    }
-                                    .frame(width: 35, height: 35)
-                                    .overlay(
-                                        Circle()
-                                            .stroke(.gray)
-                                    )
-                                    .padding(.trailing)
-                                    
-
-                                } //: HStack
-                                    .frame(maxWidth: .infinity)
-                                    .frame(height: searchBarHeight)
-                                    .overlay(
-                                        RoundedRectangle(cornerRadius: searchBarHeight)
-                                            .stroke(.gray)
-                                    )
+                            
+                        ZStack {
+                            Color.white
+                                .frame(height: searchBarHeight + 30)
+                            
+                                SearchField()
                                     .padding(.horizontal)
-                                    .background(.white)
-                                    .shadow(radius: 5)
-                            )
+                                    .padding(.top)
+                        }
+                            
                         
                         Path { path in
                             path.move(to: CGPoint(x: 0, y: 0))
@@ -90,9 +49,9 @@ struct ParkerView: View {
                             path.addLine(to: CGPoint(x: geo.size.width, y: 0))
                         }
                         .fill(.white)
+                        .frame(height: 30)
                         .shadow(radius: 5)
                         .mask(Rectangle().padding(.bottom, -20))
-                        .frame(height: 30)
                         
                         HStack {
                             Spacer()
@@ -135,7 +94,7 @@ struct ParkerView: View {
                         
                         Rectangle()
                             .foregroundColor(.white)
-                            .frame(height: 46 + geo.safeAreaInsets.bottom + tabViewDividerPadding)
+                            .frame(height: tabBarHeight + tabViewDividerPadding + geo.safeAreaInsets.bottom)
                     } //: VStack
                     .edgesIgnoringSafeArea(.bottom)
                     .frame(width: geo.size.width, height: geo.size.height + geo.safeAreaInsets.top + geo.safeAreaInsets.bottom)
@@ -194,42 +153,13 @@ struct ParkerView: View {
             .overlay(
                 Rectangle()
                     .frame(height: 1)
-                    .offset(y: (geo.size.height / 2) - geo.safeAreaInsets.top - tabViewDividerPadding)
+                    .offset(y: (geo.size.height / 2) - geo.safeAreaInsets.top - tabViewDividerPadding - 1)
                     .foregroundColor(.gray)
                     .opacity(0.7)
                 
             )
             
         } //: Geometry Reader
-    }
-}
-
-// Helper bridge to UIViewController to access enclosing UITabBarController
-// and thus its UITabBar
-struct TabBarAccessor: UIViewControllerRepresentable {
-    var callback: (UITabBar) -> Void
-    private let proxyController = ViewController()
-    
-    func makeUIViewController(context: UIViewControllerRepresentableContext<TabBarAccessor>) ->
-    UIViewController {
-        proxyController.callback = callback
-        return proxyController
-    }
-    
-    func updateUIViewController(_ uiViewController: UIViewController, context: UIViewControllerRepresentableContext<TabBarAccessor>) {
-    }
-    
-    typealias UIViewControllerType = UIViewController
-    
-    private class ViewController: UIViewController {
-        var callback: (UITabBar) -> Void = { _ in }
-        
-        override func viewWillAppear(_ animated: Bool) {
-            super.viewWillAppear(animated)
-            if let tabBar = self.tabBarController {
-                self.callback(tabBar.tabBar)
-            }
-        }
     }
 }
 
