36559953f3f9e58c28ddb9556dfc9d41cdf4e741
Ryan Smetana <ryan.smetana@xavware.com>
2023-01-10 15:21:08 -0500

    Finish major functionality of list view animations and gestures

--- uParker-SwiftUI/Views/SpotListView.swift
+++ uParker-SwiftUI/Views/SpotListView.swift
@@ -9,18 +9,24 @@ import SwiftUI
 
 struct SpotListView: View {
     // MARK: - PROPERTIES
-    @State var isExpanded: Bool = false
+    @State private var isExpanded: Bool = false
     @State private var isDragging = false
     @State private var prevDragTranslation: CGSize = CGSize.zero
+    @GestureState var isDetectingLongPress = false
     
     @Binding var viewHeight: CGFloat
     let minHeight: CGFloat
     let maxHeight: CGFloat
-    private var threshold: CGFloat {
-        return (maxHeight - minHeight) / 2
+    
+    private var viewButtonOpacity: CGFloat {
+        if viewHeight < threshold {
+            return (threshold - viewHeight) / (threshold - minHeight)
+        } else {
+            return (viewHeight - threshold) / (maxHeight - threshold)
+        }
     }
-     
-    private var cornerRad: CGFloat {
+    
+    private var listCornerRad: CGFloat {
         if viewHeight == minHeight {
             return 30
         } else if viewHeight < maxHeight {
@@ -29,6 +35,18 @@ struct SpotListView: View {
         } else {
             return 0
         }
+    }
+    
+    private var listShadowRad: CGFloat {
+        if viewHeight == maxHeight {
+            return 0
+        } else {
+            return 5
+        }
+    }
+    
+    private var threshold: CGFloat {
+        return (maxHeight - minHeight) / 2
     }
     
     var dragGesture: some Gesture {
@@ -40,26 +58,33 @@ struct SpotListView: View {
                 
                 let dragAmount = val.translation.height - prevDragTranslation.height
                 
-                if viewHeight < minHeight {
-                    viewHeight += dragAmount / 10
-                } else if viewHeight > maxHeight {
+                if viewHeight >= maxHeight && dragAmount > 0 {
                     viewHeight += 0
+                } else if viewHeight < minHeight {
+                    viewHeight += dragAmount / 10
                 } else {
                     viewHeight += dragAmount
                 }
-                print("\(30 * (viewHeight / maxHeight)) = \(cornerRad)")
+                
                 prevDragTranslation = val.translation
             }
             .onEnded { val in
                 prevDragTranslation = CGSize.zero
                 isDragging = false
                 
+                let velocity: CGFloat = val.predictedEndLocation.y - val.location.y
+                
+                if velocity > 400 {
+                    expandList()
+                } else if velocity < -400 {
+                    compressList()
+                }
+                
                 if viewHeight >= threshold {
                     expandList()
                 } else if viewHeight < threshold {
                     compressList()
                 }
-                
             }
     }
     
@@ -84,43 +109,57 @@ struct SpotListView: View {
             VStack(spacing: 8) {
                 Spacer()
                 
-                Button {
-                    if self.isExpanded {
-                        compressList()
-                    } else {
-                        expandList()
-                    }
-                } label: {
-
+                if viewHeight < threshold {
                     Text("List View")
                         .font(.footnote)
+                        .fontWeight(.semibold)
+                        .fontDesign(.rounded)
+                        .opacity(viewButtonOpacity)
                         .onTapGesture {
-                            if self.isExpanded {
-                                compressList()
-                            } else {
-                                expandList()
-                            }
+                            expandList()
                         }
                         .gesture(dragGesture)
+                } else {
+                    Button {
+                        compressList()
+                    } label: {
+                        Image(systemName: "map.fill")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 15)
+                        
+                        Text("Map")
+                            .font(.footnote)
+                    }
+                    .padding(.horizontal)
+                    .foregroundColor(.white)
+                    .fontWeight(.semibold)
+                    .fontDesign(.rounded)
+                    .frame(height: 30)
+                    .background(backgroundGradient)
+                    .clipShape(Capsule())
+                    .shadow(radius: 5)
+                    .opacity(viewButtonOpacity)
+                    .padding(.bottom)
+                    
                 }
-                .frame(height: 20)
-                .frame(maxWidth: .infinity)
-                .fontWeight(.semibold)
-                .fontDesign(.rounded)
                 
-                Capsule()
-                    .frame(width: 40, height: 6)
-                    .opacity(0.6)
-                    .padding(.bottom, 10)
-                    .gesture(dragGesture)
-
+                if viewHeight < maxHeight {
+                    Capsule()
+                        .foregroundColor(.gray)
+                        .frame(width: 40, height: 6)
+                        .opacity(self.isDragging ? 1.0 : 0.6)
+                        .padding(.bottom, 10)
+                        .gesture(dragGesture)
+                }
+                
             } //: VStack
             .frame(height: viewHeight)
             .frame(maxWidth: .infinity)
             .background (
                 Color.white
-                    .cornerRadius(cornerRad, corners: [.bottomLeft, .bottomRight])
-                    .shadow(radius: 5)
+                    .cornerRadius(listCornerRad, corners: [.bottomLeft, .bottomRight])
+                    .shadow(radius: listShadowRad)
                     .mask(Rectangle().padding(.bottom, -20))
             )
             .animation(self.isDragging ? nil : .easeInOut(duration: 0.45), value: true)
@@ -129,12 +168,12 @@ struct SpotListView: View {
             .animation(.easeInOut, value: true)
         } //: ZStack
         .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
-
+        
     } //: Body
 } //: Struct
 
 struct SpotListView_Previews: PreviewProvider {
-    @State static var viewHeight: CGFloat = 120
+    @State static var viewHeight: CGFloat = 650
     static let minHeight: CGFloat = 120
     static let maxHeight: CGFloat = 650
     static var previews: some View {
