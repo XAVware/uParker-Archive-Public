32ef1f81e7db00ca574748a8184c576e91562dc2
Ryan Smetana <ryan.smetana@xavware.com>
2023-01-20 02:28:25 -0500

    Add annotation to map.

--- uParker-SwiftUI/Views/Parker/SpotSearch/MapViewWrapper.swift
+++ uParker-SwiftUI/Views/Parker/SpotSearch/MapViewWrapper.swift
@@ -9,7 +9,6 @@ import SwiftUI
 import MapboxMaps
 import CoreLocation
 
-
 public class MapViewController: UIViewController {
     // MARK: - PROPERTIES
     internal var mapView: MapView!
@@ -17,9 +16,6 @@ public class MapViewController: UIViewController {
     var geocoder: MBGeocoder = MBGeocoder()
     
     let pennStateCoordinates: CLLocationCoordinate2D = CLLocationCoordinate2D(latitude: 40.7934, longitude: -77.8600)
-    
-    let pinView = UIHostingController(rootView: PinView())
-    
     
     // MARK: - INITIALIZER
     override public func viewDidLoad() {
@@ -54,73 +50,36 @@ public class MapViewController: UIViewController {
         // Pass options when initializing the map
         self.view.addSubview(mapView)
         self.view.sendSubviewToBack(mapView)
-        
-//        self.addViewAnnotation(at: pennStateCoordinates)
     }
     
-//    private func createSampleView(withText text: String) -> UIView {
-//        let backgroundFrame = CGRect(x: 0, y: 0, width: 100, height: 40) // Size while selected
-//
-//        let backgroundView: UIView = UIView()
-//        let priceLabel: UILabel = UILabel()
-//
-//        let unselectedBackgroundColor: UIColor = UIColor(named: "uParkerBlue")!
-//
-//        backgroundView.frame = backgroundFrame
-//        backgroundView.backgroundColor = unselectedBackgroundColor
-//        backgroundView.layer.cornerRadius = backgroundFrame.height / 2
-//
-//        backgroundView.layer.shadowRadius = 2
-//        backgroundView.layer.shadowOffset = CGSize(width: 0, height: 1)
-//        backgroundView.layer.shadowOpacity = 0.3
-//
-//        priceLabel.frame = backgroundFrame
-//        priceLabel.textColor = .white
-//        priceLabel.text = "$ \(text)"
-//        priceLabel.textAlignment = .center
-//        priceLabel.adjustsFontSizeToFitWidth = true
-//
-//        backgroundView.addSubview(priceLabel)
-//
-//        return backgroundView
-//    }
-    
     private func addViewAnnotation(at coordinate: CLLocationCoordinate2D) {
+        let pinWidth: CGFloat = 60
+        let pinHeight: CGFloat = 25
+        
         let options = ViewAnnotationOptions(
             geometry: Point(coordinate),
-            width: 100,
-            height: 40,
+            width: pinWidth,
+            height: pinHeight,
             allowOverlap: false
-//            allowOverlap: false,
-//            anchor: .center
+            //            allowOverlap: false,
+            //            anchor: .center
         )
         
+        let frame = CGRect(x: 0, y: 0, width: pinWidth, height: pinHeight)
         
-        let frame = CGRect(x: 0, y: 0, width: 70, height: 20)
-        
-        
-        let pinView: UIView = UIView(frame: frame)
+        let pinView: UILabel = UILabel(frame: frame)
+        pinView.textColor = .white
+        pinView.text = "$3.00"
+        pinView.font = .systemFont(ofSize: 12, weight: .semibold)
+        pinView.textAlignment = .center
+        pinView.adjustsFontSizeToFitWidth = true
         pinView.backgroundColor = UIColor(named: "uParkerBlue")!
-        pinView.layer.cornerRadius = frame.height
+        pinView.layer.masksToBounds = true
+        pinView.layer.cornerRadius = pinHeight / 2
         pinView.layer.shadowRadius = 2
         pinView.layer.shadowOffset = CGSize(width: 0, height: 1)
-        pinView.layer.shadowOpacity = 0.3
+        pinView.layer.shadowOpacity = 0.5
         
-        let priceLabel: UILabel = UILabel(frame: frame)
-        priceLabel.textColor = .white
-        priceLabel.text = "$3.00/Day"
-        priceLabel.font = .systemFont(ofSize: 18, weight: .semibold)
-        priceLabel.textAlignment = .center
-        priceLabel.adjustsFontSizeToFitWidth = true
-//        priceLabel.backgroundColor = UIColor(.red)
-//        priceLabel.cornerRadius = frame.height
-        priceLabel.backgroundColor = UIColor(named: "uParkerBlue")!
-        priceLabel.layer.shadowRadius = 2
-        priceLabel.layer.shadowOffset = CGSize(width: 0, height: 1)
-        priceLabel.layer.shadowOpacity = 0.3
-        
-//        pinView.addSubview(priceLabel)
-
         try? mapView.viewAnnotations.add(pinView, options: options)
     }
 }
@@ -137,13 +96,3 @@ struct MapViewWrapper: UIViewControllerRepresentable {
     }
 }
 
-struct PinView: View {
-        var body: some View {
-            Text("$3.00")
-                .font(.caption)
-                .foregroundColor(.white)
-                .background(backgroundGradient)
-                .padding()
-                .clipShape(Capsule())
-        }
-}
