e88f7ea7cfd8fbbaa2928eabae4b0db04be76b4f
Ryan Smetana <ryan.smetana@xavware.com>
2023-02-03 15:20:38 -0500

    Begin map settings view. Delete red_pin asset. Add Map Style button images.

---
+++ uParker-SwiftUI/Views/Parker/SpotSearch/MapSettingsView.swift
@@ -0,0 +1,20 @@
+//
+//  MapSettingsView.swift
+//  uParker-SwiftUI
+//
+//  Created by Smetana, Ryan on 2/3/23.
+//
+
+import SwiftUI
+
+struct MapSettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MapSettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapSettingsView()
+    }
+}
