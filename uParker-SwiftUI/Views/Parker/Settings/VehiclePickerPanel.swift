9f528124fa99b144ea45c29588cb14d3d6b34398
Ryan Smetana <ryan.smetana@xavware.com>
2023-02-21 16:06:26 -0500

    Adjust make list on change of selected year.

--- uParker-SwiftUI/Views/Parker/Settings/VehiclePickerPanel.swift
+++ uParker-SwiftUI/Views/Parker/Settings/VehiclePickerPanel.swift
@@ -7,15 +7,41 @@
 
 import SwiftUI
 
-class VehicleJSONManager: ObservableObject {
+final class VehicleJSONManager: ObservableObject {
     
-    @Published var vehicleList = [VehicleOption]()
-    @Published var yearOptions = Set<Int>()
-    @Published var makeOptions = [String]()
-    @Published var modelOptions = [String]()
+    @Published var vehicleList = [VehicleOption]() {
+        didSet {
+            yearOptions = Set(vehicleList.map { $0.year })
+        }
+    }
+    
+    @Published var yearOptions = Set<Int>() {
+        didSet {
+            selectedYear = yearOptions.max() ?? 0
+        }
+    }
+    
+    @Published var makeOptions = Set<String>()
+    @Published var modelOptions = Set<String>()
+    
+    @Published var selectedYear: Int = 0 {
+        didSet {
+            let makesForYear = vehicleList.filter{ $0.year == selectedYear }
+            makeOptions = Set(makesForYear.map { $0.make })
+            selectedMake = makeOptions.sorted{$0 < $1}[0]
+        }
+    }
+    
+    @Published var selectedMake: String = ""
     
     init() {
+        print("Initialized")
         load()
+//        selectedYear = yearOptions.sorted{$0 > $1}[0]
+//
+//        print("Makes for year: \(selectedYear)")
+//        let makesForYear: [VehicleOption] = vehicleList.filter { $0.year == selectedYear }
+        
     }
     
     func load() {
@@ -26,9 +52,15 @@ class VehicleJSONManager: ObservableObject {
                 if let data = data {
                     let allVehicles = try JSONDecoder().decode([VehicleOption].self, from: data)
                     
+                    let yearOptions = Set(allVehicles.map { $0.year })
+                    let mostRecentYear = yearOptions.max()
+                    
+                    let makesForYear = allVehicles.filter{ $0.year == mostRecentYear }
+                    let makeOptions = Set(makesForYear.map { $0.make })
+                    
                     DispatchQueue.main.async {
                         self.vehicleList = allVehicles
-                        self.yearOptions = Set(allVehicles.map { $0.year })
+//                        self.yearOptions = Set(allVehicles.map { $0.year })
                     }
                 } else {
                     print("No Data")
@@ -45,7 +77,7 @@ struct VehiclePickerPanel: View {
     @Binding var newVehicle: Vehicle?
     @ObservedObject var vehicles = VehicleJSONManager()
     
-    @State var selectedYear: Int?
+//    @State var selectedYear: Int = 2022
     
     // MARK: - BODY
     var body: some View {
@@ -56,20 +88,16 @@ struct VehiclePickerPanel: View {
                 
                 Spacer()
                 
-                Picker("", selection: $selectedYear) {
+                Picker("", selection: $vehicles.selectedYear) {
                     ForEach(vehicles.yearOptions.sorted{$0 > $1}, id: \.self) { year in
                         Text(String(year))
                     }
                 }
                 
             } //: HStack
-            .onTapGesture {
-                print("Tapped")
-                print(selectedYear ?? "Empty")
-            }
-            .onChange(of: vehicles.yearOptions) { _ in
-                self.selectedYear = vehicles.yearOptions.sorted{$0 > $1}[0]
-            }
+//            .onChange(of: vehicles.yearOptions) { _ in
+//                self.selectedYear = vehicles.yearOptions.sorted{$0 > $1}[0]
+//            }
             
             HStack {
                 Text("Make:")
@@ -77,8 +105,11 @@ struct VehiclePickerPanel: View {
                 
                 Spacer()
                 
-                Text(self.newVehicle?.make ?? "Empty")
-                    .modifier(TextMod(.title3, .regular))
+                Picker("", selection: $vehicles.selectedMake) {
+                    ForEach(vehicles.makeOptions.sorted{$0 < $1}, id: \.self) { make in
+                        Text(String(make))
+                    }
+                }
             } //: HStack
             
             
