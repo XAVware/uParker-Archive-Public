a593bffd5aa9d0bfe70eea826b0aa3a310253f0d
Ryan Smetana <ryan.smetana@xavware.com>
2023-02-23 20:13:32 -0500

    Begin implementing view model for vehicles view

--- uParker-SwiftUI/App/Constants.swift
+++ uParker-SwiftUI/App/Constants.swift
@@ -71,6 +71,4 @@ let states = [  "---Select State---",
                 "WY - Wyoming"
 ]
 
-let stateAbbreviations = [
-    "AK",    "AL",    "AR",    "AZ",    "CA",    "CO",    "CT",    "DE",    "FL",    "GA",    "HI",    "IA",    "ID",    "IL",    "IN",    "KS",    "KY",    "LA",    "MA",    "MD",    "ME",    "MI",    "MN",    "MO",    "MS",    "MT",    "NC",    "ND",    "NE",    "NH",    "NJ",    "NM",    "NV",    "NY",    "OH",    "OK",    "OR",    "PA",    "RI",    "SC",    "SD",    "TN",    "TX",    "UT",    "VA",    "VT",    "WA",   "WI",    "WV",    "WY"
-]
+let stateAbbreviations = [ "AK", "AL", "AR", "AZ", "CA", "CO", "CT", "DE", "FL", "GA", "HI", "IA", "ID", "IL", "IN", "KS", "KY", "LA", "MA", "MD", "ME", "MI", "MN", "MO", "MS", "MT", "NC", "ND", "NE", "NH", "NJ", "NM", "NV", "NY", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VA", "VT", "WA", "WI", "WV", "WY"]
