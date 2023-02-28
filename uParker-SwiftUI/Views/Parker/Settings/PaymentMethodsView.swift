f62e6d0e065cfe96a0874b96aadfa1088f462bfa
Ryan Smetana <ryan.smetana@xavware.com>
2023-02-28 18:56:10 -0500

    Begin Payment Methods UI

--- uParker-SwiftUI/Views/Parker/Settings/PaymentMethodsView.swift
+++ uParker-SwiftUI/Views/Parker/Settings/PaymentMethodsView.swift
@@ -13,14 +13,74 @@ struct PaymentMethodsView: View {
     
     // MARK: - BODY
     var body: some View {
-        Text("Hello World")
+        ScrollView {
+            VStack {
+                VStack {
+                    HStack {
+                        Text("Credit Cards")
+                            .modifier(TextMod(.title3, .semibold))
+                        
+                        Spacer()
+                        
+                        Button {
+                            //
+                        } label: {
+                            Image(systemName: "plus.circle")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 15)
+                            
+                            Text("Add Card")
+                        } //: Add Card Button
+                    } //: HStack
+                    
+                    Divider()
+                        .padding(.bottom)
+                    
+                    Button {
+                        //
+                    } label: {
+                        VStack {
+                            HStack {
+                                VStack(alignment: .leading, spacing: 8) {
+                                    Text("**** 6906")
+                                        .modifier(TextMod(.title3, .semibold))
+                                    
+                                    Text("Exp: 03/16")
+                                } //: VStack
+                                
+                                Spacer()
+                                
+                                Image(systemName: "chevron.right")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(height: 15)
+                            } //: HStack
+                            
+                            Divider()
+                        } //: VStack
+                    } //: Card Button
+                    .padding(.horizontal)
+                    
+                } //: VStack
+                .padding()
+                .padding(.top, 32)
+                
+                
+                Spacer()
+            } //: VStack
+            .navigationTitle("Payment Methods")
+            .navigationBarTitleDisplayMode(.automatic)
+        } //: Scroll
     }
     
 }
 
 struct PaymentMethodsView_Previews: PreviewProvider {
     static var previews: some View {
-        PaymentMethodsView()
+        NavigationView {
+            PaymentMethodsView()
+        }
     }
 }
 
